d48daa417719f5da52926dc66802a9d463a762c7
tuh8888 <harrison.pielke-lombardo@cuanschutz.edu>
2020-05-22 21:22:24 -0600

    Added magit dependency.

--- chezmoi.el
+++ chezmoi.el
@@ -3,7 +3,7 @@
 ;; Author: Harrison Pielke-Lombardo
 ;; Maintainer: Harrison Pielke-Lombardo
 ;; Version: 1.0.0
-;; Package-Requires: ((emacs "24.3"))
+;; Package-Requires: ((emacs "24.3") magit)
 ;; Homepage: http://www.github.com/tuh8888/chezmoi.el
 ;; Keywords: vc
 
@@ -29,6 +29,8 @@
 ;; commentary
 
 ;;; Code:
+
+(require 'magit)
 
 (defvar chezmoi|selected-file)
 
