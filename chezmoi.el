e8027b1189f9a773043f52cc1885130a8bc7e15c
Harrison Pielke-Lombardo <harrison.pielke-lombardo@cuanschutz.edu>
2022-01-30 19:18:07 -0700

    Renamed fn's according to kebab-case.

--- chezmoi.el
+++ chezmoi.el
@@ -33,39 +33,39 @@
 (require 'magit)
 (require 'cl-lib)
 
-(defvar chezmoi|buffer-target-file)
+(defvar chezmoi-buffer-target-file)
 
-(defun chezmoi|select-file (files prompt f)
+(defun chezmoi-select-file (files prompt f)
   "Call F on the selected file from FILES, giving PROMPT."
   (funcall f (completing-read prompt files)))
 
-(defun chezmoi|source-file (target-file)
+(defun chezmoi-source-file (target-file)
   "Return the source file corresponding to TARGET-FILE."
   (let* ((cmd (concat "chezmoi source-path " (when target-file (shell-quote-argument target-file))))
          (result (shell-command-to-string cmd))
          (files (split-string result "\n")))
     (cl-first files)))
 
-(defun chezmoi|managed ()
+(defun chezmoi-managed ()
   "List all files and directories managed by chezmoi."
   (let* ((cmd "chezmoi managed")
          (result (shell-command-to-string cmd))
          (files (split-string result "\n")))
     (cl-map 'list (lambda (file) (concat "~/" file)) files)))
 
-(defun chezmoi|managed-files ()
+(defun chezmoi-managed-files ()
   "List only files managed by chezmoi."
-  (cl-remove-if #'file-directory-p (chezmoi|managed)))
+  (cl-remove-if #'file-directory-p (chezmoi-managed)))
 
-(defun chezmoi|write (target-file)
+(defun chezmoi-write (target-file)
   "Run =chezmoi apply= on the TARGET-FILE.
 This overwrites the target with the source state."
-  (interactive (list chezmoi|buffer-target-file))
+  (interactive (list chezmoi-buffer-target-file))
   (if (= 0 (shell-command (concat "chezmoi apply " target-file)))
       (message "Wrote %s" target-file)
     (message "Failed to write file")))
 
-(defun chezmoi|diff (arg)
+(defun chezmoi-diff (arg)
   "View output of =chezmoi diff= in a diff-buffer.
 If ARG is non-nil, switch to the diff-buffer."
   (interactive "i")
@@ -79,10 +79,10 @@ If ARG is non-nil, switch to the diff-buffer."
       (whitespace-mode 0))
     b))
 
-(defun chezmoi|changed-files ()
+(defun chezmoi-changed-files ()
   "Use chezmoi diff to return the files that have changed."
   (let ((line-beg nil))
-    (with-current-buffer (chezmoi|diff t)
+    (with-current-buffer (chezmoi-diff t)
       (goto-char (point-max))
       (let ((files nil))
         (while (setq line-beg (re-search-backward "^\\+\\{3\\} .*" nil t))
@@ -93,81 +93,81 @@ If ARG is non-nil, switch to the diff-buffer."
             (push (concat "~" file-name) files)))
         files))))
 
-(defun chezmoi|find ()
+(defun chezmoi-find ()
   "Edit a source file managed by chezmoi.
 If the target file has the same state as the source file,add a hook to
 'save-buffer' that applies the source state to the target state. This way, when
 the buffer editing the source state is saved the target state is kept in sync.
 Note: Does not run =chezmoi edit="
   (interactive)
-  (chezmoi|select-file (chezmoi|managed-files)
+  (chezmoi-select-file (chezmoi-managed-files)
                        "Select a dotfile to edit: "
                        (lambda (file)
-                         (find-file (chezmoi|source-file file))
+                         (find-file (chezmoi-source-file file))
                          (let ((mode (assoc-default
                                       (file-name-nondirectory file)
                                       auto-mode-alist
                                       'string-match)))
                            (when mode (funcall mode)))
                          (message file)
-                         (unless (member file (chezmoi|changed-files))
+                         (unless (member file (chezmoi-changed-files))
                            (add-hook 'after-save-hook
-                                     (lambda () (chezmoi|write file)) 0 t))
-                         (setq-local chezmoi|buffer-target-file file))))
+                                     (lambda () (chezmoi-write file)) 0 t))
+                         (setq-local chezmoi-buffer-target-file file))))
 
-(defun chezmoi|ediff ()
+(defun chezmoi-ediff ()
   "Choose a dotfile to merge with its source using ediff.
 Note: Does not run =chezmoi merge=."
   (interactive)
-  (chezmoi|select-file (chezmoi|changed-files)
+  (chezmoi-select-file (chezmoi-changed-files)
                        "Select a dotfile to merge: "
                        (lambda (file)
-                         (ediff-files (chezmoi|source-file file) file))))
+                         (ediff-files (chezmoi-source-file file) file))))
 
-(defun chezmoi|magit-status ()
+(defun chezmoi-magit-status ()
   "Show the status of the chezmoi source repository."
   (interactive)
-  (magit-status-setup-buffer (chezmoi|source-file nil)))
+  (magit-status-setup-buffer (chezmoi-source-file nil)))
 
-(defun chezmoi|select-files (files prompt f)
+(defun chezmoi-select-files (files prompt f)
   "Iteratively select file from FILES given PROMPT and apply F to each selected.
 Selected files are removed after they are selected."
   (let ((files (cl-copy-list files)))
     (while files
-      (chezmoi|select-file files
+      (chezmoi-select-file files
                            (concat prompt " (C-g to stop): ")
                            (lambda (file)
                              (funcall f file)
                              (setq files (remove file files)))))))
 
-(defun chezmoi|write-from-target (target-file)
+(defun chezmoi-write-from-target (target-file)
   "Apply the TARGET-FILE's state to the source file buffer.
 Useful for files which are autogenerated outside of chezmoi."
-  (interactive (list chezmoi|buffer-target-file))
-  (with-current-buffer (find-file-noselect (chezmoi|source-file target-file))
+  (interactive (list chezmoi-buffer-target-file))
+  (with-current-buffer (find-file-noselect (chezmoi-source-file target-file))
     (replace-buffer-contents (find-file-noselect target-file))))
 
-(defun chezmoi|write-files ()
+(defun chezmoi-write-files ()
   "Force overwrite multiple dotfiles with their source state."
   (interactive)
-  (chezmoi|select-files (chezmoi|changed-files)
+  (chezmoi-select-files (chezmoi-changed-files)
                         "Select dotfile to apply source state changes"
-                        #'chezmoi|write))
+                        #'chezmoi-write))
 
 
-(defun chezmoi|write-files-from-target ()
+(defun chezmoi-write-files-from-target ()
   "Force overwrite the source state of multiple dotfiles with their target state."
   (interactive)
-  (chezmoi|select-files (chezmoi|changed-files)
+  (chezmoi-select-files (chezmoi-changed-files)
                         "Select a dotfile to overwrite its source state with target state"
-                        #'chezmoi|write-from-target))
+                        #'chezmoi-write-from-target))
 
-(defun chezmoi|open-target ()
+(defun chezmoi-open-target ()
   "Open buffer's target file."
   (interactive)
-  (find-file chezmoi|buffer-target-file))
+  (find-file chezmoi-buffer-target-file))
 
-(defun chezmoi|dired-add-marked-files ()
+(defun chezmoi-dired-add-marked-files ()
   "Add files marked in Dired to source state."
   (interactive)
   (dolist (file (dired-get-marked-files))
